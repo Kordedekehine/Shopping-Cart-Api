b8d891ecb5cabf944b792e40d6c6b3ddb1ddace4
amigoscode <salamikehinde345@gmail.com>
2022-06-17 13:19:38 +0100

    initial set-up

---
+++ src/test/java/ShoppingCartsAPI/carts/CartsApplicationTests.java
@@ -0,0 +1,13 @@
+package ShoppingCartsAPI.carts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CartsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
