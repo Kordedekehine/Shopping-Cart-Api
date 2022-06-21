758c0599130fd29b70ffa345b15315502ea8bb23
amigoscode <salamikehinde345@gmail.com>
2022-06-21 22:50:51 +0100

    set up the wish list controller

---
+++ src/main/java/ShoppingCartsAPI/carts/controller/WishListController.java
@@ -0,0 +1,48 @@
+package ShoppingCartsAPI.carts.controller;
+
+import ShoppingCartsAPI.carts.api.ApiResponse;
+import ShoppingCartsAPI.carts.dtos.product.ProductDto;
+import ShoppingCartsAPI.carts.model.Product;
+import ShoppingCartsAPI.carts.model.User;
+import ShoppingCartsAPI.carts.model.WishList;
+import ShoppingCartsAPI.carts.services.AuthenticationServices;
+import ShoppingCartsAPI.carts.services.ProductService;
+import ShoppingCartsAPI.carts.services.WishListServices;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/wishlist")
+public class WishListController {
+
+    @Autowired
+    WishListServices wishListServices;
+
+    @Autowired
+    AuthenticationServices authenticationServices;
+
+    @GetMapping("/{token}")
+    public ResponseEntity<List<ProductDto>> getWishList(@PathVariable("token") String token){
+        int userID = authenticationServices.getUser(token).getId();
+        List<WishList> body = wishListServices.findAllWishList(userID);
+        List<ProductDto> products = new ArrayList<>();
+        for (WishList wishList : body) {
+            products.add(ProductService.getDtoFromProduct(wishList.getProduct()));
+        }
+         return new ResponseEntity<>(products, HttpStatus.OK);
+    }
+
+    @PostMapping("/addwishes")
+    public ResponseEntity<ApiResponse> addWishList(@RequestBody Product product,@RequestParam("token") String token){
+      authenticationServices.authenticate(token);
+        User user = authenticationServices.getUser(token);
+        WishList wishList = new WishList(user,product);
+        wishListServices.addWishes(wishList);
+       return new ResponseEntity<>(new ApiResponse(true,"wishes successfully registered"),HttpStatus.OK);
+    }
+}
--- src/main/java/ShoppingCartsAPI/carts/model/WishList.java
+++ src/main/java/ShoppingCartsAPI/carts/model/WishList.java
@@ -22,4 +22,41 @@ public class WishList {
     @ManyToOne()
     @JoinColumn(name = "product_id")
     private Product product;
+
+    public WishList(User user, Product product) {
+        this.user = user;
+        this.product = product;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public Date getCreatedDate() {
+        return createdDate;
+    }
+
+    public void setCreatedDate(Date createdDate) {
+        this.createdDate = createdDate;
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
 }
